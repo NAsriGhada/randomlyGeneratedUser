fc1d86bc49b12d0308f30a095298d5c7c7a5cc84
ghada <nasrighada10@outlook.com>
2023-06-13 00:48:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import data from "./data.json";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 function App() {
+  const randomIndex = Math.floor(Math.random() * data.length);
+  const randomUser = data[randomIndex];
+  useEffect(() => {
+    console.log(randomIndex);
+    console.log(randomUser);
+  }, []);
+  const [color, setColor] = useState("rgba(0, 0, 0, 0.87)");
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    setColor(color === "rgba(0, 0, 0, 0.87)" ? "red" : "rgba(0, 0, 0, 0.87)");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia component="img" alt="image" image={randomUser.picture} />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            id: {randomUser.id} <br />
+            balance: {randomUser.balance}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            age: {randomUser.age} <br />
+            name: {randomUser.name} <br />
+            gender: {randomUser.gender} <br />
+            company: {randomUser.company} <br />
+            email: {randomUser.email}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+        <FavoriteIcon style={{ color: color }} onClick={handleClick} />
+      </Card>
     </div>
   );
 }
